4ec1011999d25384ec48c664e0e05d6d6c96c63b
Anggie Castillo <anniecastillono@outlook.com>
2023-11-03 10:45:52 +0100

    done

--- coasters-project/server.js
+++ coasters-project/server.js
@@ -1,1 +1,21 @@
-// Happy coding! Party! 
+const express = require('express')
+
+const app = express()
+
+app.use(express.static('public'))
+app.set('views', `${__dirname}/views`)
+app.set('view engine', 'hbs')
+
+
+require('./db/database-connection')
+
+app.get('/', (req, res) => {
+    res.render('home-page')
+})
+app.get('/CoastersGarelly', (req, res) => {
+    .find()
+        .then(coaster => res.render('coaster-page', { coaster: coaster }))
+        .catch(err => console.log('ERROR:', err))
+})
+
+app.listen(3000, () => console.log('Server listening on port 3000'))
